d86d11e05bfe30eb79390e2b56e0212d425ddc78
Nathan Billaud <nathan.billaud@iia-formation.fr>
2024-12-09 16:50:29 +0100

    question fixtures

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -6,7 +6,7 @@ use App\Repository\QuestionsRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: QuestionsRepository::class)]
-class Questions
+class Question
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
