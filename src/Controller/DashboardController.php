db13270964141737c9ec2d4578eaf9067a042e5b
Nathan Billaud <nathan.billaud@iia-formation.fr>
2024-12-10 16:33:30 +0100

    fixtures question finis

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,9 @@ use Symfony\Component\Routing\Attribute\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'dashboard')]
-    public function index(): Response
+    public function index(EntityManagerInterface $manager): Response
     {
+
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
         ]);
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Question;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,17 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+         for ($i = 10; $i > 0; $i--) {
+            $question = new Question();
+            $question->setEntitled("{$i} x 17");
+            $question->setResponse($i * 17);
+            $timestart= $i*10;
+            $timeend= $timestart -10;
+            $question->setDateStart(new \DateTimeImmutable("now -{$timestart} minutes"));
+            $question->setDateEnd(new \DateTimeImmutable("now -{$timeend} minutes"));
+             $manager->persist($question);
+         }
+
 
         $manager->flush();
     }
